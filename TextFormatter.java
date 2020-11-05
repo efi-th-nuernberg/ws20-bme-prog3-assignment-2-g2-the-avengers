793402f5598772e56eab158ee3831187964d227f
repl.it user <replituser@example.com>
2020-11-05 16:10:12 +0000

    update

--- TextFormatter.java
+++ TextFormatter.java
@@ -12,16 +12,24 @@ class TextFormatter {
 
     public static void main(String[] args) {
         TextFormatter formatter = new TextFormatter(40);
-        formatter.print(text);
-    } //Konstruktor
+        formatter.print(text,0);
+        // ENTER 0 for left formatted, 1 for right formatted
+    } 
 
     public TextFormatter(int maxLineLength) {
         lineLength = maxLineLength;
     }
 
-    public void print(String aText) {
+    public String addSpaces(int maxSpaces){
+      String spaces = "";
+      for (int i = 0; i< maxSpaces; i++){
+          spaces += " ";
+      }
+      return spaces;
+    }
+    public void print(String aText,int format) {
 
-      static int LIMIT_BACK = 7;
+      final int LIMIT_BACK = 14;
       
         int lineLengthCounter = 0;
         while (true) {
@@ -29,9 +37,9 @@ class TextFormatter {
                 for (int lastSpaceChar = 0; lastSpaceChar < LIMIT_BACK; lastSpaceChar++) {
                     if ((lineLengthCounter == lineLength - lastSpaceChar) && (aText.charAt(lineLength - lastSpaceChar) == ' ')) {
                         if (aText.charAt(0) == ' ') {
-                            System.out.println(aText.substring(1, lineLengthCounter));
+                            System.out.println(addSpaces(lastSpaceChar*format)+ aText.substring(1, lineLengthCounter));
                         } else {
-                            System.out.println(aText.substring(0, lineLengthCounter));
+                            System.out.println(addSpaces(lastSpaceChar*format)+aText.substring(0, lineLengthCounter));
                         }
                         aText = aText.substring(lineLengthCounter, aText.length());
                         lineLengthCounter = 0;
@@ -42,6 +50,8 @@ class TextFormatter {
                 break;
             }
         }
-        System.out.println(aText.substring(1, aText.length()));
+
+        System.out.println(addSpaces((lineLength-aText.length())*format)+aText.substring(1, aText.length()));
     }
+    // test
 }
